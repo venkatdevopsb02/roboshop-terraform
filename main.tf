728c71698a29d04b4fbf47afa2e48f021a87323a
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-03-14 11:17:44 +0000

    Mar 14, 2023, 7:17 AM

--- main.tf
+++ main.tf
@@ -12,6 +12,13 @@ resource "azurerm_virtual_network" "az-trainings" {
   address_space       = ["10.0.0.0/16"]
 }
 
+resource "azurerm_subnet" "az-trainings" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.az-trainings.name
+  virtual_network_name = azurerm_virtual_network.az-trainings.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
 
 resource "azurerm_network_security_group" "az-trainings" {
   name                = "nsg-az-trainings"
