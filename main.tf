705010e2d6175caaf16f2c8ab7ecce31370c72bf
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-03-14 10:59:35 +0000

    Mar 14, 2023, 6:59 AM

--- main.tf
+++ main.tf
@@ -11,3 +11,52 @@ resource "azurerm_virtual_network" "az-trainings" {
   location            = azurerm_resource_group.az-trainings.location
   address_space       = ["10.0.0.0/16"]
 }
+
+
+resource "azurerm_network_security_group" "az-trainings" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.az-trainings.location
+  resource_group_name = azurerm_resource_group.az-trainings.name
+}
+
+resource "azurerm_network_security_rule" "az-trainings" {
+  name                        = "allow-ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.az-trainings.name
+  network_security_group_name = azurerm_network_security_group.az-trainings.name
+}
+
+resource "azurerm_network_security_rule" "az-trainings" {
+  name                        = "allow-80"
+  priority                    = 101
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.az-trainings.name
+  network_security_group_name = azurerm_network_security_group.az-trainings.name
+}
+
+resource "azurerm_network_security_rule" "az-trainings" {
+  name                        = "allow-8080"
+  priority                    = 102
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "8080"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.az-trainings.name
+  network_security_group_name = azurerm_network_security_group.az-trainings.name
+}
