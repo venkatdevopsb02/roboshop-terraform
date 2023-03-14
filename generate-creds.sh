a3df559128d96763fcc1432bf7c94625622b70cb
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-03-14 10:52:01 +0000

    Mar 14, 2023, 6:52 AM

--- generate-creds.sh
+++ generate-creds.sh
@@ -1,1 +1,4 @@
 az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/d8391944-774c-41db-95d3-6797158258bc"
+terraform init 
+terraform plan -var-file="creds.tfvars"
+terraform apply -var-file="creds.tfvars"
