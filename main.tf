32e143cc8a4ceac2ea1fc9ece289c013d1e48805
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-03-15 10:30:38 +0000

    Mar 15, 2023, 6:30 AM

--- main.tf
+++ main.tf
@@ -4,3 +4,53 @@ resource "azurerm_resource_group" "az-trainings" {
   location = "Canada Central"
 }
 
+
+resource "azurerm_public_ip" "az-trainings" {
+  name                = "jumpserver-pip"
+  location            = azurerm_resource_group.az-trainings.location
+  resource_group_name = azurerm_resource_group.az-trainings.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "az-trainings" {
+  name                = "az-trainings-nic"
+  location            = azurerm_resource_group.az-trainings.location
+  resource_group_name = azurerm_resource_group.az-trainings.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.az-trainings.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.az-trainings.id
+  }
+}
+
+
+
+resource "azurerm_linux_virtual_machine" "az-trainings" {
+  name                = "jumpserver"
+  resource_group_name = azurerm_resource_group.az-trainings.name
+  location            = azurerm_resource_group.az-trainings.location
+  size                = "Standard_B1s"
+  admin_username      = "centos"
+  admin_password      = "DeVOps654321"
+
+  disable_password_authentication = false 
+
+  network_interface_ids = [
+    azurerm_network_interface.az-trainings.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+
+  source_image_reference {
+    publisher = "OpenLogic"
+    offer     = "CentOS-LVM"
+    sku       = "8-lvm-gen2"
+    version   = "8.5.2022101401"
+  }
+}
