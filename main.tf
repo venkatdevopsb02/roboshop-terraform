86e54d1d55f3e5a64dbbe7f1c6711329126037e7
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-03-16 10:33:39 +0000

    Mar 16, 2023, 6:33 AM

--- main.tf
+++ main.tf
@@ -19,5 +19,5 @@ module "linuxservers" {
   rg-name             = azurerm_resource_group.az-trainings.name
   location            = azurerm_resource_group.az-trainings.location
   subnet-id           = azurerm_subnet.az-trainings.id
-  nic-name            = ${each-value}-nic
+  nic-name            = nic-${each-value}
 }
