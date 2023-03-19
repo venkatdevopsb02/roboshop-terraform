87b13d8ac27b5800ad45d8ecc7dee84d8bd2f1a7
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-03-19 10:51:39 +0000

    Mar 19, 2023, 6:51 AM

--- output.tf
+++ output.tf
@@ -5,9 +5,12 @@ output "sample" {
 }
 
 
-output "network_interface_private_ip" {
-  description = "private ip addresses of the vm nics"
-  value       = module.publicipadd.azurerm_network_interface.*
-  #value       = module.publicipadd.azurerm_network_interface.az-trainings.*.private_ip_address  
-}
+#output "network_interface_private_ip" {
+#  description = "private ip addresses of the vm nics"
+#  value       =  module.publicipadd.azurerm_network_interface.*
+#  #value       = module.publicipadd.azurerm_network_interface.az-trainings.*.private_ip_address  
+#}
 
+output "public_ip_address" {
+  value = module.publicipadd.azurerm_linux_virtual_machine.*
+}
